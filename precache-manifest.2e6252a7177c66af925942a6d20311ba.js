c875ed59fe91306e3b75f337e5dce067255355a5
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-26 20:36:00 -0600

    Updates

---
+++ precache-manifest.2e6252a7177c66af925942a6d20311ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ee6a5b5afc5c546a51193b253229bd",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "8b97497ceb594acf3ffa",
+    "url": "/react-pokedex/static/css/main.4bfd7811.chunk.css"
+  },
+  {
+    "revision": "6252ffcc6f054eaf5c05",
+    "url": "/react-pokedex/static/js/2.bff40374.chunk.js"
+  },
+  {
+    "revision": "14b6951eb3599e7c836b2179fd9b2b8b",
+    "url": "/react-pokedex/static/js/2.bff40374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b97497ceb594acf3ffa",
+    "url": "/react-pokedex/static/js/main.ff9d6250.chunk.js"
+  },
+  {
+    "revision": "d696704d547ed4302e08",
+    "url": "/react-pokedex/static/js/runtime-main.843eca1b.js"
+  }
+]);
